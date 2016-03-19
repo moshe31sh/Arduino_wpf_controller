19515da799b3bb34784d019e7e20add6a650629a
moshe <moshe31sh@gmail.com>
2016-03-19 23:15:33 +0200

    First commit

---
+++ Arduino/ArduinoCom.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO.Ports;
+using System.IO;
+
+namespace Arduino
+{
+    class ArduinoCom
+    {
+        private SerialPort currentPort;
+        private String port;
+        private int rate;
+         
+        /// <summary>
+        /// Property
+        /// </summary>
+        public String Port
+        {
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value)){
+                    this.port = value;
+                }
+            }
+            get{
+                return port;
+            }
+        }
+
+        public int Rate
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    this.rate = value;
+                }
+                else
+                {
+                    this.rate = 0;
+                }
+            }
+            get
+            {
+                return this.rate;
+            }
+        }
+
+        public void connect()
+        {
+            this.currentPort = new SerialPort();
+            this.currentPort.PortName = Port;
+            this.currentPort.BaudRate = Rate;
+            try
+            {
+                this.currentPort.Open();
+                this.currentPort.Write("3");
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Data);
+            }
+            finally
+            {
+                this.currentPort.Close();
+            }
+        }        
+
+    }
+}
---
+++ Arduino/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Arduino
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ArduinoCom arduino;
+        // Available baud rates
+        private int[] baudrates = {
+            4800,
+            9600,
+            14400,
+            19200,
+            28800,
+            38400,
+            57600,
+            115200
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            init();
+            arduino = new ArduinoCom();
+
+        }
+        private void init()
+        {
+            string[] ports = System.IO.Ports.SerialPort.GetPortNames();
+            foreach (String port in ports)
+            {
+                this.Ports.Items.Add(port);
+            }
+            foreach (int rate in baudrates)
+            {
+                this.Rates.Items.Add(rate);
+            }
+
+        }
+
+        private void Dc_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            this.arduino.Port = Ports.SelectedItem.ToString();
+            this.arduino.Rate = Int32.Parse(Rates.SelectedItem.ToString());
+      
+
+
+             this.arduino.connect();
+        }
+
+    }
+}
